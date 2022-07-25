54052d0a2cfb6ffb2b59781e59413b96dfdbdc0a
MichaelZeroo <michaelsngh99@gmail.com>
2022-07-25 21:13:43 +1200

    coomit

--- screens/Billrequest.js
+++ screens/Billrequest.js
@@ -24,11 +24,11 @@ const Billrequest = () => {
   
   return (
     <View style={styles.container}>
-      <Text>Billrequest</Text>
-      <Text>If you have a bill left to pay, you can request your bill here and you will get an email with your bill</Text>
-      <Text>Please enter these details to get your bill</Text>
+      <Text style={styles.header}>Bill request</Text>
+      <Text style={styles.header2}>If you have a bill left to pay, you can request your bill here and you will get an email with your bill</Text>
+      <Text style={styles.header2}>Please enter these details to get your bill</Text>
 
-      <View>
+      <View style={styles.buttonContainer}>
       <TouchableOpacity
         onPress={sendMail}
         style={[styles.button]}>
@@ -43,5 +43,40 @@ const Billrequest = () => {
 export default Billrequest
 
 const styles = StyleSheet.create({
-
+container:{
+  flex: 1,
+  backgroundColor: 'white',
+},
+header:{
+  color:'#115367',
+  fontSize:40,
+  paddingBottom:20,
+  paddingLeft: 20,
+  paddingRight: 20,
+  fontWeight: 'bold',
+},
+header2:{
+  color:'#115367',
+  fontSize:20,
+  padding:10,
+},
+button:{
+  backgroundColor: '#33838c',
+width: '70%',
+padding: 15,
+borderRadius: 10,
+alignItems: 'center',
+},
+buttonText:{
+  color: 'white',
+fontWeight: '700',
+fontSize: 16,
+},
+buttonContainer:{
+        width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+    
+    },
 })
